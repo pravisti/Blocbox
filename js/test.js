9454ecb83a8496435a6e767ec17f73decab1581f
Pravisti <pravisti@gmail.com>
2016-03-16 11:00:52 -0700

    start free trial button fixed

---
+++ js/test.js
@@ -0,0 +1,12 @@
+var parkIsOpen = true;
+if (parkIsOpen) {
+  console.log ( "Welcome to the Badlands National Park! Try to enjoy your stay.");
+} else {
+  console.log ( "Sorry, the badlands are particularly bad today. We're closed!");
+}
+
+var parkIsOpen = true;
+if(parkIsOpen) {
+console.log("Welcome to the Badlands National Park! Try to enjoy your stay."); }else{
+console.log("Sorry, the Badlands are particularly bad today. We're closed!");
+}
